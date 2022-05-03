fe7b92ed2dfcdcbacaaf3448a75bfc0e672c473e
krishna <krishgithub10@gmail.com>
2022-05-03 13:14:37 +0530

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return (
         <div className="header">
             <img src={logo} alt="logo" className="logo" />
-            <h3> my travel journal. </h3>
+            <h3> mytravel journal. </h3>
         </div>
     );
 }
